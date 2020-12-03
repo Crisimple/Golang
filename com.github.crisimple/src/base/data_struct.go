6cebdab82377b8ab0254f3234c1d5b0d793a4391
crisimple <crisimple@qq.com>
2020-12-03 21:30:23 +0800

    data_struct map struct

--- com.github.crisimple/src/base/data_struct.go
+++ com.github.crisimple/src/base/data_struct.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"sort"
+	"time"
+)
 
 // 初始化数组时可以使用初始化列表来设置数组元素的值
 func initArray1()  {
@@ -148,8 +153,6 @@ func SectionExpress()  {
 	t := b[1:3:5]
 	fmt.Printf("t:%v\n len(t)%v\n cap(t)%v", t, len(t), cap(t))
 }
-
-
 // 使用make()函数动态构造切片
 func SectionMake()  {
 	a := make([]int, 3, 10)
@@ -157,9 +160,6 @@ func SectionMake()  {
 	fmt.Println("len(a): ", len(a))
 	fmt.Println("cap(a): ", cap(a))
 }
-
-
-
 // --------------------------- 切片的操作 ------------------------------
 // 切片的比较
 func sectionCompare()  {
@@ -191,8 +191,6 @@ func sectionAssignmentCopy()  {
 	fmt.Println("b: ", b)
 
 }
-
-
 // 切片的遍历
 func sectionForRange()  {
 	s := []int{1, 3, 5}
@@ -234,31 +232,149 @@ func sectionDelete()  {
 }
 
 
+// ---------------- map -----------------
+func mapUse()  {
+	// map 的定义赋值
+	scoreMap := make(map[string]int, 8)
+	scoreMap["A"] = 100
+	scoreMap["B"] = 90
+	fmt.Println(scoreMap)
+	fmt.Println(scoreMap["A"])
+	fmt.Printf("type of a:%T\n", scoreMap)
+	userInfo := map[string]string{
+		"A": "A1",
+		"B": "B",
+	}
+	fmt.Println("userInfo: ", userInfo)
+}
+// 判断某个键是否存在
+func mapJudgeKey()  {
+	userInfo := map[string]string{
+		"A": "A1",
+		"B": "B",
+	}
+	v, ok := userInfo["A"]
+	if ok {
+		fmt.Println(v)
+	} else {
+		fmt.Println("查无此人")
+	}
+}
+// map的遍历
+func forMap()  {
+	tempMap := make(map[string]string)
+	tempMap["A"] = "A1"
+	tempMap["B"] = "B1"
+	tempMap["C"] = "C1"
+	// 获取键和值
+	for k, v := range tempMap{
+		fmt.Println(k, "---", v)
+	}
+	// 获取键
+	for k := range tempMap{
+		fmt.Println(k)
+	}
+	// 按指定顺序遍历map
+	rand.Seed(time.Now().UnixNano()) //初始化随机数种子
+	var TempMap = make(map[string]int, 200)
+	for i := 0; i < 100; i++ {
+		key := fmt.Sprintf("stu%02d", i) //生成stu开头的字符串
+		value := rand.Intn(100)          //生成0~99的随机整数
+		TempMap[key] = value
+	}
+	// 取出map中的所有key存入切片keys
+	var keys = make([]string, 0,  200)
+	for key := range TempMap{
+		keys = append(keys, key)
+	}
+	// 对切片进行排序
+	sort.Strings(keys)
+	//按照排序后的key遍历map
+	for _, key := range keys {
+		fmt.Println(key, TempMap[key])
+	}
+}
+// delete()函数，删除键值对
+func deleteKeyValue()  {
+	tempMap := make(map[string]int)
+	tempMap["A"] = 10
+	tempMap["B"] = 20
+	tempMap["C"] = 30
+	for k, v := range tempMap{
+		fmt.Println(k, "1---1", v)
+	}
+	fmt.Println("\n")
+	delete(tempMap, "B")
+	for k, v := range tempMap{
+		fmt.Println(k, "2---2", v)
+	}
+}
+//元素为map类型的切片
+func valueMapSection()  {
+	var sectionMap = make(map[string]string, 3)
+	for index, value := range sectionMap{
+		fmt.Printf("index:%v\n value:%v\n\n\n", index, value)
+	}
+	fmt.Println("after init")
+	// 对切片中的map元素进行初始化
+	sectionMap = make(map[string]string, 10)
+	sectionMap["name"] = "XT"
+	sectionMap["password"] = "123456"
+	sectionMap["address"] = "LZ"
+	for index, value := range sectionMap {
+		fmt.Printf("index:%d\n value:%v\n", index, value)
+	}
+}
+// 值为切片类型的map
+func valueSection()  {
+	var sliceMap = make(map[string][]string, 3)
+	fmt.Println(sliceMap)
+	fmt.Println("after init")
+	key := "中国"
+	value, ok := sliceMap[key]
+	if !ok {
+		value = make([]string, 0, 2)
+	}
+	value = append(value, "北京", "上海")
+	sliceMap[key] = value
+	fmt.Println(sliceMap)
+}
+
 
 //func main()  {
-	//initArray1()
-	//initArray2()
-	//initArray3()
-
-	//indexArray()
-
-	//multiArray()
-
-	//copyArray()
-
-	//a := [3]int{1, 2, 3}
-	//fmt.Println(arraySum(a))
-
-	//knowSection()
-	//SectionExpress()
-	//SectionMake()
-
-	//sectionCompare()
-	//sectionAssignmentCopy()
-
-	//sectionForRange()
-
-	//sectionCrud()
-
-//	sectionDelete()
+//	//initArray1()
+//	//initArray2()
+//	//initArray3()
+//
+//	//indexArray()
+//
+//	//multiArray()
+//
+//	//copyArray()
+//
+//	//a := [3]int{1, 2, 3}
+//	//fmt.Println(arraySum(a))
+//
+//	// ------------- 切片 -------------
+//	//knowSection()
+//	//SectionExpress()
+//	//SectionMake()
+//
+//	//sectionCompare()
+//	//sectionAssignmentCopy()
+//
+//	//sectionForRange()
+//
+//	//sectionCrud()
+//
+////	sectionDelete()
+//
+//	// --------------- map --------------
+//	//mapUse()
+//	//mapJudgeKey()
+//	//forMap()
+//	//deleteKeyValue()
+//	valueMapSection()
+//	fmt.Println("------")
+//	valueSection()
 //}
